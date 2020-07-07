7169143a8b0ac5d51539580a2438a06092832693
Shivam1904 <shivam.sriv93@gmail.com>
2020-07-07 08:48:48 -0400

    (conf): Added requiremnets and other confs.

---
+++ settings/openpose.py
@@ -0,0 +1,43 @@
+COCO_ANNOTATION = [
+    "Nose_X", "Nose_Y", "Nose_C",
+    "Neck_X", "Neck_Y", "Neck_C",
+    "RShoulder_X", "RShoulder_Y", "RShoulder_C",
+    "RElbow_X", "RElbow_Y", "RElbow_C",
+    "RWrist_X", "RWrist_Y", "RWrist_C",
+    "LShoulder_X", "LShoulder_Y", "LShoulder_C",
+    "LElbow_X", "LElbow_Y", "LElbow_C",
+    "LWrist_X", "LWrist_Y", "LWrist_C",
+    "RHip_X", "RHip_Y", "RHip_C",
+    "RKnee_X", "RKnee_Y", "RKnee_C",
+    "RAnkle_X", "RAnkle_Y", "RAnkle_C",
+    "LHip_X", "LHip_Y", "LHip_C",
+    "LKnee_X", "LKnee_Y", "LKnee_C",
+    "LAnkle_X", "LAnkle_Y", "LAnkle_C",
+    "REye_X", "REye_Y", "REye_C",
+    "LEye_X", "LEye_Y", "LEye_C",
+    "REar_X", "REar_Y", "REar_C",
+    "LEar_X", "LEar_Y", "LEar_C"
+]
+
+USEFUL_ANNOTATIONS = [
+    "Nose_X", "Nose_Y",
+    "Neck_X", "Neck_Y",
+    "RShoulder_X", "RShoulder_Y",
+    "RElbow_X", "RElbow_Y",
+    "RWrist_X", "RWrist_Y",
+    "LShoulder_X", "LShoulder_Y",
+    "LElbow_X", "LElbow_Y",
+    "LWrist_X", "LWrist_Y",
+    "REye_X", "REye_Y",
+    "LEye_X", "LEye_Y",
+    "REar_X", "REar_Y",
+    "LEar_X", "LEar_Y"
+]
+
+CONF_ANNOTATION = [
+    "Nose_C",
+    "REye_C",
+    "LEye_C",
+    "REar_C",
+    "LEar_C"
+]
